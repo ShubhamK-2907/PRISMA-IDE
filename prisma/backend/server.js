61ec77db78e8c40488b4a2127aa8653d6f20607d
“ShubhamK-2907” <shubhamk2907@gmail.com>
2024-09-19 03:10:16 +0530

    Initial FE + BE Repo Setup

---
+++ prisma/backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+export default app;
